9211f3cabfad973f5fc23888588910ebebe9067b
David White <whited75@gmail.com>
2015-02-15 17:11:33 -0700

    Finish stack implementation

--- stack.rb
+++ stack.rb
@@ -4,6 +4,7 @@ require_relative 'linked_list'
 
 class Stack
   class UnderflowError < StandardError; end
+
   def initialize
     @stack = LinkedList.new
   end
@@ -18,22 +19,29 @@ class Stack
   # Raises an error if the stack is empty (called a "stack underflow")
   # O(1) time
   def pop
-    @stack.shift
+    if @stack.empty?
+      raise UnderflowError, "Stack is empty"
+    else
+      @stack.shift
+    end
   end
 
   # Return the item on the top of the stack without removing it
   # O(1) time
   def peek
+    @stack.head
   end
 
   # Return true if the stack is empty and false otherwise
   # O(1) time
   def empty?
+    @stack.empty?
   end
 
   # Return the number of items on the stack
   # O(1) time
   def size
+    @stack.length
   end
 end
 
