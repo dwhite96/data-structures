8e50b49a9e6c61868c3843ff7812723eb9481038
David White <whited75@gmail.com>
2015-02-18 22:09:25 -0700

    MinMaxStack implementation

--- minmax_stack.rb
+++ minmax_stack.rb
@@ -3,38 +3,68 @@ require_relative 'linked_list'
 # Implement a "min-max stack" - a stack that has contant-time
 # "maximum" _and_ "minimum" operations.
 
-
 class MinMaxStack
+  class UnderflowError < StandardError; end
+
   def initialize
+    @list = LinkedList.new
+    @current_max = 0
   end
 
-  # Places +item+ on the top of the stack
   def push(item)
+    value = []
+    if empty?
+      @current_min = item
+      value << @current_min << @current_min
+    elsif item < @current_min
+      @current_min = item
+      value << @current_min << @current_min
+    else
+      value << item << @current_min
+    end
+    if item > @current_max
+      @current_max = item
+      value << @current_max
+    else
+      value << @current_max
+    end
+    @list.unshift(value)
   end
 
-  # Removes the item on the top of the stack and returns it.
-  # Raises an error if the stack is empty (called a "stack underflow")
   def pop
+    fail UnderflowError, "Stack is empty" if empty?
+    @list.shift.first
   end
 
-  # Return the item on the top of the stack without removing it
   def peek
+    empty? ? @list.head.value : @list.head.value.first
   end
 
-  # Return true if the stack is empty and false otherwise
   def empty?
+    @list.empty?
   end
 
-  # Return the number of items on the stack
   def size
+    @list.length
   end
 
   # Returns the smallest item on the stack
+  # O(1) time
   def min
+    if empty?
+      @current_min = nil
+    else
+      @current_min = @list.head.value[1]
+    end
   end
 
   # Returns the largest item on the stack
   # O(1) time
   def max
+    if empty?
+      @current_max = 0
+    else
+      @current_max = @list.head.value.last
+    end
   end
 end
