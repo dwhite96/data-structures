1417f7577eeb6ac68fe46e65e53302adc0572bde
David White <whited75@gmail.com>
2015-03-08 14:55:39 -0600

    First working version of binary search tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,3 +1,5 @@
+require "pp"
+
 require_relative "binary_tree"
 
 # Implement a binary search tree.
@@ -9,4 +11,53 @@ require_relative "binary_tree"
 #   empty?              O(1) time
 
 class BinarySearchTree < BinaryTree
+  def include?(value)
+    self.each do |node|
+      if node == value
+        return true
+      else
+        next
+      end
+    end
+  end
+
+  def insert(value)
+    if @value == :empty
+      @value = value
+    elsif value < @value
+      if @left.class == EmptyTree
+        @left = BinarySearchTree.new(value)
+      else
+        @left.insert(value)
+      end
+    else value > @value
+      if @right.class == EmptyTree
+        @right = BinarySearchTree.new(value)
+      else
+        @right.insert(value)
+      end
+    end
+
+    self
+  end
+
+  def empty?
+    self.value.nil?
+  end
 end
+
+b = BinarySearchTree.new(9)
+b.insert(17)
+b.insert(54)
+b.insert(32)
+b.insert(13)
+b.insert(4)
+b.insert(5)
+b.insert(5)
+b.insert(78)
+b.insert(29)
+
+PP.pp(b)
+
+p b.include?(32)
+p b.include?(6)
