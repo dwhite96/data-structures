4250f8277171e7b252c808ee4436e7ce3abc2a16
David White <whited75@gmail.com>
2015-03-09 22:24:25 -0600

    Fisher-Yate's shuffle algorithm attempt

--- array_shuffle.rb
+++ array_shuffle.rb
@@ -5,20 +5,15 @@
 # The shuffled array could look the same as the input array
 # The larger the input array the higher the likelihood you will get a different array
 
-# Modern Fisher-Yates Shuffle Algorithm (Richard Durstenfeld's version)
+# Modern Fisher-Yate's Shuffle Algorithm (Richard Durstenfeld's version)
 
 class ArrayShuffle
   def shuffle(list)
     n = list.length
     n.downto(1) do |i|
-      p i
-      b = list[i..-1]
       i -= 1
-      a = list[0..i]
-      print a, b
       j = rand(0..i)
-      p j + 1
-      if list[0..i].include?(j + 1)
+      if list[0..i].include?(j + 1)  # Not sure how this may affect arrays already shuffled
         list[j], list[i] = list[i], list[j]
       else
         next
