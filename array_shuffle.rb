2e44bc8bd90ea60756d3687411ad6ab44d24b659
David White <whited75@gmail.com>
2015-03-12 19:10:34 -0600

    Add method for creating array copy using Object#dup

--- array_shuffle.rb
+++ array_shuffle.rb
@@ -1,19 +1,32 @@
-# Cannot create a temporary array or make copies of the input array
-# Must move the elements in place
-# Two elements may swap places
-# Shouldn't be able to predict what the shuffled array looks like
-# The shuffled array could look the same as the input array
-# The larger the input array the higher the likelihood you will get a different array
-
 # Modern Fisher-Yate's Shuffle Algorithm (Richard Durstenfeld's version)
+#   http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 
-class ArrayShuffle
-  def shuffle(list)
-    n = list.length
-    (n - 1).downto(0) do |i|
-      j = rand(0..i)
-      list[j], list[i] = list[i], list[j]
-    end
-    list
+def shuffle!(list)
+  n = list.length
+
+  (n - 1).downto(0) do |i|
+    j = rand(0..i)
+    list[j], list[i] = list[i], list[j]
+  end
+
+  list
+end
+
+# This is one example of where the Object#dup method is appropriately used.
+# This method should be avoided in most cases.
+# Object#dup may be appropriate in a custom reverse method as well.
+
+def shuffle(list)
+  shuffle!(list.dup)
+end
+
+# Example usage:
+#   ruby shuffle.rb 1000 1,2,3,4 | ruby histogram.rb
+if __FILE__ == $PROGRAM_NAME
+  sample_count = ARGV[0].to_i
+  values       = ARGV[1].split(",")
+
+  sample_count.times do
+    puts shuffle(values).join("-")
   end
 end
