d5906a1a9b16266a93968fab7db3c1619197d44a
David White <whited75@gmail.com>
2015-03-22 14:16:15 -0600

    Finish BinarySearchTree#remove(value)

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -9,29 +9,39 @@ require_relative "binary_tree"
 #   empty?              O(1) time
 
 class BinarySearchTree < BinaryTree
+  def self.empty
+    EmptyBinarySearchTree.new
+  end
+
+  def initialize(value, left = :empty, right = :empty)
+    @value = value
+    @left = EmptyBinarySearchTree.new(left)
+    @right = EmptyBinarySearchTree.new(right)
+  end
+
   def include?(value)
-    return true if value == @value
-    if value < @value
-      return false if @left.empty?
-      @left.include?(value)
-    elsif value > @value
-      return false if @right.empty?
-      @right.include?(value)
+    return true if value == self.value
+    if value < self.value
+      return false if self.left.empty?
+      self.left.include?(value)
+    elsif value > self.value
+      return false if self.right.empty?
+      self.right.include?(value)
     end
   end
 
   def insert(value)
-    if value < @value
-      if @left.empty?
-        @left = BinarySearchTree.new(value)
+    if value < self.value
+      if self.left.empty?
+        self.left = BinarySearchTree.new(value)
       else
-        @left.insert(value)
+        self.left.insert(value)
       end
-    elsif value > @value
-      if @right.empty?
-        @right = BinarySearchTree.new(value)
+    elsif value > self.value
+      if self.right.empty?
+        self.right = BinarySearchTree.new(value)
       else
-        @right.insert(value)
+        self.right.insert(value)
       end
     end
 
@@ -43,31 +53,77 @@ class BinarySearchTree < BinaryTree
   #   EmptyTree if leaf or with left node if that node holds a value.
 
   def remove(value)
-    if value == @value
-      @value = self.left.max.value  # Mutates existing object
-      self.left.max = EmptyTree.new  # This doesn't work
-    elsif value < @value
-      return false if @left.empty? # Need to change this line to raise error
-      @left.remove(value)
-    elsif value > @value
-      return false if @right.empty?
-      @right.remove(value)
+    if value < self.value
+      self.left = self.left.remove(value) if self.has_left_child?
+      return self
+    elsif value > self.value
+      self.right = self.right.remove(value) if self.has_right_child?
+      return self
     end
 
-    self
+    # +self+ is the node we want to remove
+    case self.children_count
+    when 0
+      BinarySearchTree.empty
+    when 1
+      if self.has_left_child?
+        self.left
+      elsif self.has_right_child?
+        self.right
+      end
+    when 2
+      left_max   = self.left.max.value
+      self.value = left_max
+      self.left  = left.remove(left_max)
+      self
+    end
+  end
+
+  def has_left_child?
+    self.left.empty? ? false : true
+  end
+
+  def has_right_child?
+    self.right.empty? ? false : true
   end
 
   def empty?
     false
   end
 
+  def children_count
+    if left.empty? && right.empty?
+      0
+    elsif left.empty? || right.empty?
+      1
+    elsif !left.empty? && !right.empty?
+      2
+    end
+  end
+
   def min
-    return self if @left.empty?
-    @left.max
+    return self if self.left.empty?
+    self.left.max
   end
 
   def max
-    return self if @right.empty?
-    @right.max
+    return self if self.right.empty?
+    self.right.max
+  end
+end
+
+class EmptyBinarySearchTree < BinarySearchTree
+  def initialize(value = :empty, left = :empty, right = :empty)
+    @value = :empty
+    @left  = :empty
+    @right = :empty
+  end
+
+  def min; end
+
+  def max; end
+
+  def empty?
+    true
   end
 end
